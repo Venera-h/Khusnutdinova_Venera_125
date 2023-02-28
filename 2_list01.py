65941920411383fa8d5113dd0ed956febe1af363
Венера <husnutdinova2003@mail.ru>
2023-02-28 12:54:26 +0300

    Create 2_list01

---
+++ 2_list01.py
@@ -0,0 +1,10 @@
+import random 
+numbers_1 =[]
+numbers_2 =[]
+for i in range(50,101):
+    numbers_1.append(random.randint(0,10000))
+    numbers_2.append(random.randint(0,10000))
+print(numbers_1)
+print(numbers_2)
+
+    
