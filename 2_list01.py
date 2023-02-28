0ffb285013367cf3d1f94a019f0bd3caf3d3e9da
Венера <husnutdinova2003@mail.ru>
2023-02-28 22:29:04 +0300

    /

--- 2_list01.py
+++ 2_list01.py
@@ -2,7 +2,6 @@ import random
 import collections
 numbers_1 =[]
 numbers_2 =[]
-numbers_3 = []
 for i in range(50,101):
     numbers_1.append(random.randint(0,10000))
     numbers_2.append(random.randint(0,10000))
--- 3_list02.py
+++ 3_list02.py
@@ -1,12 +1,10 @@
 import random 
-import collections
 numbers_1 =[]
 numbers_2 =[]
-numbers_3 = []
 for i in range(50,101):
     numbers_1.append(random.randint(0,10000))
     numbers_2.append(random.randint(0,10000))
-print(numbers_1)
-print(numbers_2)
-numbers_3 = [j for j in numbers_1 if j in numbers_2]
-print(numbers_3)
+print(sorted(numbers_1))
+print(sorted(numbers_2))
+numbers_3 = list(set(numbers_1 + numbers_2))
+print(sorted(numbers_3))
