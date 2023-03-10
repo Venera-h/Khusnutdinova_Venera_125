1e1e5fb3ac8cb3a3fdc7ce04ca0f477692dfe4f7
Венера <husnutdinova2003@mail.ru>
2023-03-10 09:05:36 +0300

    7_line_01

--- json/vze.py
+++ json/vze.py
@@ -1,8 +1,8 @@
 import json
-
-with open('file1.json', 'r') as read_file:
-    dict_obj= json.load(read_file)
+# Здесь я пробовала использовать разные папка, но тоже без результатно
+#path = "C:\Users\Home\Downloads\text.json"
+path = "C:\\Users\Home\Documents\GitHub\Khusnutdinova_Venera_125\json\json_text.json"
+with open(path, 'r') as f:
+    dict_obj = json.load(f.read())
     print(dict_obj)
 
-#with open("people.json") as f:
- #   data = json.load(f)
