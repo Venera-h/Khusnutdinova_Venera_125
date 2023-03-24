6cf00cb20ff65a1e31d837c6d9dedd523d723982
Венера <husnutdinova2003@mail.ru>
2023-03-24 23:06:37 +0300

    12 средняя

--- Средние задачи/11_work_with_text_01.py
+++ Средние задачи/11_work_with_text_01.py
@@ -1,14 +1,24 @@
+# не доделано
+
+
 import collections
+print("Введите текст")
 text = str(input())
 words = text.split()
 
 replay={}
-for word in words:
-    if word in replay:
-        replay[word] += 1
-    else:
-        replay[word]= 1
+if len(words) <5:
+    print("Write other text")
+else:
+    for word in words:
+        if word in replay:
+            replay[word] += 1
+        else:
+            replay[word]= 1
 
 max_word = max(replay, key=replay.get)
+max_len = max(words, key = len)
+
+print(max_word, max_len)
 
 
--- Средние задачи/12_function_01.py
+++ Средние задачи/12_function_01.py
@@ -1,15 +1,12 @@
 def sum_range(start, end):
     if start > end:
-       # start, end = end, start
-    start1 =end
-
-    end1 = start
+       start, end = end, start
+    #start1 =end
+    #end1 = start
     sum = 0
     for i in range(start, end + 1):
         sum += i
     return sum
-
-
 
 start = int(input("Введите значение 1 :"))
 end = int(input("Введите значение 2 :"))
