7a52444b01d6ff9defa048ad92411be97183371c
Венера <husnutdinova2003@mail.ru>
2023-03-25 08:40:21 +0300

    14  средняя

--- Средние задачи/14/14_play.py
+++ Средние задачи/14/14_play.py
@@ -1,3 +1,19 @@
 import random
 number = random.randint(1,101)
+name = input("Введите ваше имя")
+print("Привет!", name, " Я загадал число от 1 до 100, которое тебе нужно отгадать")
 
+attempt = 0
+while True:
+    figure = int(input("Введите число"))
+    attempt +=1
+
+    if number>figure:
+        print("Подсказка: число больше вашего ответа")
+    if number<figure:
+        print("Подсказка: число меньше вашего ответа")
+    if number==figure:
+        print("Ты угадал!")
+        with open("game.txt","a") as f:
+            f.write(f"\n{name}, ты угадал число за {attempt} попыток!.\n Правильное число - {number}\n")
+            break
