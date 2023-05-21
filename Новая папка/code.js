5e1aa81a07363f5228826b8944ae9f1c5a4ad13c
Венера <husnutdinova2003@mail.ru>
2023-05-21 14:23:57 +0300

    фея + движение

---
+++ Новая папка/code.js
@@ -0,0 +1,10 @@
+var removeItem = document.getElementsByClassName('btn-danger')
+
+class ShoppingCart{
+
+//- name: a string representing the name of the item
+//- description: a string describing the item
+//- price: a number representing the price of the item
+//- quantity: a number representing the quantity of the item in the cart
+var 
+}
