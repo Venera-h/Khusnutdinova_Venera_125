c39ea8eb6f8deb337f407c4dd5a06f4b06b08184
Венера <husnutdinova2003@mail.ru>
2023-05-31 20:39:21 +0300

    феечка добавлена!!!

--- game/main_code.js
+++ game/main_code.js
@@ -1,63 +1,66 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
-canvas.width= window.innerHeight
-document.body.appendChild(canvas);
+
+canvas.width= window.innerWidth
+canvas.height= window.innerHeight
 
 class Player {
     constructor() {
-        this.position ={
-            x: 100,
-            y: 100
+        this.position = {
+            x: 200,
+            y: 200
         }
-        // this.element = document.createElement("img");
-        // this.element.src = url(image/img.png);
-        // this.element.style.width = "78px";
-        // this.element.style.height = "86px";
-        // document.body.appendChild(this.element);
+        this.velocity = {
+            x:0,
+            y:0
+        }
+
+        const image = new Image()
+        image.src ='./image/img.png'
+
+        this.image = image
+        this.width = 100
+        this.height = 100
   }
-        
+    draw(){
+        c.drawImage(this.image, this.position.x, this.position.y)
+    }
     
     }
-// const fairy = document.getElementById("fairy");
 
-// document.addEventListener("keydown", function(event){
-//     jump();
-// });
 
-// function jump () {
-//     if (fairy.classList != "jump" ){
-//         fairy.classList.add("jump")
-//     }
-//     setTimeout(function(){
-//         fairy.classList.remove("jump")
-//     },200)
+// addEventListener('keydown', ({ keyCode }) => {
+//         switch (keyCode) {
+//             case 65:
+//                 console.log('left')
+//                 break
+//             case 83:
+//                 console.log('down')
+//                 break    
+//             case 68:
+//                 console.log('right')
+//                 break
+//             case 87:
+//                 console.log('up')
+//                 break
+//         }
+//     })
+
+// class Platform{
+//     this.position = {
+//         x: 0, 
+//         y: 0
+//      }
+//     this.width = 200
+//     this.height = 20
 // }
-
-
-addEventListener('keydown', ({ keyCode }) => {
-        switch (keyCode) {
-            case 65:
-                console.log('left')
-                break
-            case 83:
-                console.log('down')
-                break    
-            case 68:
-                console.log('right')
-                break
-            case 87:
-                console.log('up')
-                break
-        }
-    })
-
-class Platform{
-    this.position ={
-        x: 0, 
-        y: 0
-            }
-    this.width: 200;
-    this.height: 20;
-}
 const player = new Player()
-const platform = new Platform()
+player.draw()
+// const platform = new Platform()
+
+function animate() {
+    requestAnimationFrame(animate)
+   c.fillRect(0,0, canvas.width, canvas.height)
+   player.draw()
+}
+animate()
